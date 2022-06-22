c7e34f223b2a7328b42a365cc32a71b4fea3dbe5
Adam Sisk <adamsisk91@gmail.com>
2022-06-22 14:13:44 -0500

    adds css support

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@calamityadam/component-library-poc';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button label="it worked!" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
